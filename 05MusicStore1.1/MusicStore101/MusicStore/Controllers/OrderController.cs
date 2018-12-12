d01cfef7e4880c3707a6ce65c6901ec9833e8457
mwailxx <1650098394@qq.com>
2018-12-12 18:41:03 +0800

    12-12下午1

--- 05MusicStore1.1/MusicStore101/MusicStore/Controllers/OrderController.cs
+++ 05MusicStore1.1/MusicStore101/MusicStore/Controllers/OrderController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MusicStoreEntity;
+using MusicStore.ViewModels;
 
 namespace MusicStore.Controllers
 {
@@ -16,18 +17,42 @@ namespace MusicStore.Controllers
         public ActionResult Buy()
         {
             //1.确认用户是否登录 是否登录过期
-
+            if (Session["LoginUserSessionModel"] == null)
+                return RedirectToAction("login", "Account", new { weturnUrl = Url.Action("Buy", "Order") });
             //2.查询出当前用户Person 查询该用户的购物车项
-
+            var person = (Session["LoginUserSessionModel"] as LoginUserSessionModel).Person;
+            var carts = _context.Carts.Where(x => x.Person.ID == x.Person.ID).ToList;
+            //算购物车的总价
+            decimal? totalPrice = (from item in carts select item.Count * item.Album.Price).Sum();//linq表达式一句完成
             //3.创建新Order对象
-
+            var order = new Order()
+            {
+                AddressPerson = person.Name,
+                MobilNumber = person.MobileNumber,
+                Person = _context.Persons.Find(person.ID),
+                TotalPrice = totalPrice ?? 0.00M,
+            };
             //4.把购物项导入订单明细
-
+            order.OrderDetails = new List<OrderDetail>();
+            foreach(var item in carts)
+            {
+                var detail = new OrderDetail()
+                {
+                    AlbumID = item.AlbumID,
+                    Album = _context.Albums.Find(item.Album.ID),
+                    Count = item.Count,
+                    Price = item.Album.Price
+                };
+                order.OrderDetails.Add(detail);
+            }
             //5.将订单和明细在视图呈现，验证用户收件人、地址、电话、供用户选择确认要购买专辑
 
+            //当前订单未持久化，用会话保存方便用户进行编辑
+            Session["Order"] = order;
             return View();
         }
 
+        [HttpPost]
         public ActionResult RemoveDetail(Guid id)
         {
             return Json("");
@@ -37,5 +62,19 @@ namespace MusicStore.Controllers
         ///处理用户提交下单
         ///</summary>
         ///<param name="oder"></param> 
+        [HttpPost]
+        public ActionResult Buy(Order oder)
+        {
+            return View();
+        }
+
+        ///<summary>
+        ///浏览用户订单列表
+        ///</summary>
+        ///<returns></returns>
+          public ActionResult Index()
+        {
+            return View();
+        }
     }
 }
--- 05MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
+++ 05MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MusicStoreEntity.UserAndRole;
+using System.ComponentModel.DataAnnotations;
 
 namespace MusicStoreEntity
 {
@@ -15,13 +16,23 @@ namespace MusicStoreEntity
         public Guid ID { get; set; }
         public DateTime OrderDateTime { get; set; }//订单的时间
         public virtual Person Perdon { get; set; }//所属用户
+
+        [Display(Name = "收件人")]
+        [Required(ErrorMessage = "收件人不能为空。")]
         public string AddressPerson { get; set; }//收件人
+
+        [Display(Name = "收件人地址")]
+        [Required(ErrorMessage = "收件人地址不能为空。")]
         public string Address { get; set; }//收件人地址
+
+        [Display(Name = "手机")]
+        [Required(ErrorMessage = "手机不能为空。")]
         public string MobilNumber { get; set; }//收件人的手机
         public decimal TotalPrice { get; set; }//总价
         public string TradeNo { get; set; }//支付流水号
         public bool PaySuccess { get; set; }//是否支付成功
 
+        public virtual EnumOrderStatus EnumOrderStatus { get; set; }//订单状态
         //购买专辑明细,EF中的包含关系
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
 
@@ -29,7 +40,7 @@ namespace MusicStoreEntity
         {
             ID = Guid.NewGuid();
             OrderDateTime = DateTime.Now;
-            TradeNo = OrderDateTime.ToString"yyyMMddhhhmmssffff");
+            TradeNo = OrderDateTime.ToString（"yyyMMddhhhmmssffff");
         }
     }
 }
