16c5d447372aba2b168a61751694704e3b1a3896
mwailxx <1650098394@qq.com>
2018-12-13 10:46:16 +0800

    12-13确认订单

---
+++ 05MusicStore1.1/MusicStore101/MusicStore/LockedHelp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicStore
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class LockedHelp
+    {
+        public static Mutex _Mutex =
+    }
+    /// <summary>
+    /// 解锁
+    /// </summary>
+    /// <param name="id">主键</param>
+    public static void ThreadUnLocked(Guid id)
+    {
+        var slim = _Slim[id];
+        if(_Count[id] == 1)
+
+    }
+}
--- 05MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
+++ 05MusicStore1.1/MusicStore101/MusicStoreEntity/Order.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MusicStoreEntity.UserAndRole;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
 
 namespace MusicStoreEntity
 {
@@ -13,7 +14,10 @@ namespace MusicStoreEntity
     /// </summary>
     public class Order
     {
+        [ScaffoldColumn(false)]
         public Guid ID { get; set; }
+
+        [ScaffoldColumn(false)]
         public DateTime OrderDateTime { get; set; }//订单的时间
         public virtual Person Perdon { get; set; }//所属用户
 
@@ -28,10 +32,17 @@ namespace MusicStoreEntity
         [Display(Name = "手机")]
         [Required(ErrorMessage = "手机不能为空。")]
         public string MobilNumber { get; set; }//收件人的手机
+
+        [ScaffoldColumn(false)]
         public decimal TotalPrice { get; set; }//总价
+
+        [ScaffoldColumn(false)]
         public string TradeNo { get; set; }//支付流水号
+
+        [ScaffoldColumn(false)]
         public bool PaySuccess { get; set; }//是否支付成功
 
+        [ScaffoldColumn(false)]
         public virtual EnumOrderStatus EnumOrderStatus { get; set; }//订单状态
         //购买专辑明细,EF中的包含关系
         public virtual ICollection<OrderDetail> OrderDetails { get; set; }
